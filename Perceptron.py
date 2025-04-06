8be66acacce347f7ee504670b1588e98969faa2d
h4rad4 <williamhrd4@gmail.com>
2025-04-05 20:30:45 -0400

    Correção no perceptron
    1. Os pesos são inicializados entre -0.5 e 0.5;
    2. Exemplo de uso do perceptron e da função de ativação adicionado no notebook de treino.

--- Perceptron.py
+++ Perceptron.py
@@ -1,7 +1,8 @@
 
 import numpy as np
+from Activation import Degree
 
 class Perceptron:
-    # w: vetor de pesos
     def __init__(self):
-        self.w = np.random.uniform(-5,5,)
+        self.w= np.random.uniform(-0.5, 0.5, 3)  # atributos + bias
+        self.learning_rate= 0.1
