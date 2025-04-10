a81bc1b0aee4d284af8cd7662a490fa8a4f783ac
Geovana <geovana.matias1@gmail.com>
2025-04-10 10:24:24 -0400

    Ajuste de Erro do atibuto do perceptron

--- .ipynb_checkpoints/Perceptron-checkpoint.py
+++ .ipynb_checkpoints/Perceptron-checkpoint.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class Perceptron:
     def __init__(self):
-        self._w= np.random.uniform(-0.5, 0.5, 3)  # atributos + bias
+        self.w= np.random.uniform(-0.5, 0.5, 3)  # atributos + bias
         self.learning_rate= 0.1
         
     def set_w(self, new_w):
