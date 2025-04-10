81e098ac98a616a2c77b0f5d986053e1b321750c
Geovana <geovana.matias1@gmail.com>
2025-04-10 10:33:25 -0400

    Ajuste de Erro do atibuto do perceptron

--- Perceptron.py
+++ Perceptron.py
@@ -5,14 +5,4 @@ class Perceptron:
         self.w= np.random.uniform(-0.5, 0.5, 3)  # atributos + bias
         self.learning_rate= 0.1
         
-    def set_w(self, new_w):
-        if isinstance(new_w, np.ndarray) and new_w.shape == self._w.shape: 
-            self._w = new_w
-        else:
-            raise ValueError("O peso deve ser um array NumPy do mesmo tamanho que self._w!")
-            
-    def set_learning_rate(self, new_learning_rate):
-        if isinstance(new_learning_rate, float): 
-            self.learning_rate = self.learning_rate
-        else:
-            raise ValueError("Valor inválido")
+    
